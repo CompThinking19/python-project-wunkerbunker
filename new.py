18bae91a05824b133ca9deaac2eb80a527857d56
wunkerbunker <55245089+wunkerbunker@users.noreply.github.com>
2019-12-04 12:18:16 -0500

    updated ==
    yep

--- new.py
+++ new.py
@@ -2,55 +2,55 @@ import simpleaudio as sa
 
 print('Using one word, type how you are feeling')
 x = input()
-if x = input(happy)
+if x == input(happy)
 #Cmaj
     filename = 'happy.wav'
     wave_obj = sa.WaveObject.from_wave_file(happy)
     play_obj = wave_obj.play()
     play_obj.wait_done()
-if x = input(sad)
+if x == input(sad)
 #Cmin
     filename = 'sad.wav'
     wave_obj = sa.WaveObject.from_wave_file(sad)
     play_obj = wave_obj.play()
     play_obj.wait_done()
-if x = input(angry)
+if x == input(angry)
 #Cdim7
     filename = 'angry.wav'
     wave_obj = sa.WaveObject.from_wave_file(angry)
     play_obj = wave_obj.play()
     play_obj.wait_done()
-if x = input(surprised)
+if x == input(surprised)
 #CminMaj7
     filename = 'surprised.wav'
     wave_obj = sa.WaveObject.from_wave_file(surprised)
     play_obj = wave_obj.play()
     play_obj.wait_done()
-if x = input(triumphant)
+if x == input(triumphant)
 #E/Dmaj9
     filename = 'triumphant.wav'
     wave_obj = sa.WaveObject.from_wave_file(happy)
     play_obj = wave_obj.play()
     play_obj.wait_done()
-if x = input(melancholy)
+if x == input(melancholy)
 #BMaj13
     filename = 'melancholy.wav'
     wave_obj = sa.WaveObject.from_wave_file(melancholy)
     play_obj = wave_obj.play()
     play_obj.wait_done()
-if x = input(romantic)
+if x == input(romantic)
 #Cmaj7
     filename = 'romantic.wav'
     wave_obj = sa.WaveObject.from_wave_file(romantic)
     play_obj = wave_obj.play()
     play_obj.wait_done()
-if x = input(optimistic)
+if x == input(optimistic)
 #C9
     filename = 'optimistic.wav'
     wave_obj = sa.WaveObject.from_wave_file(optimistic)
     play_obj = wave_obj.play()
     play_obj.wait_done()
-if x = input(seinfeld)
+if x == input(seinfeld)
 #C9
     filename = 'seinfeld.wav'
     wave_obj = sa.WaveObject.from_wave_file(seinfeld)
