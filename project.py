deeb223ad427bdedb749c9cb117e878997e9095a
wunkerbunker <55245089+wunkerbunker@users.noreply.github.com>
2019-12-04 12:15:54 -0500

    hi
    hi again

---
+++ project.py
@@ -0,0 +1,22 @@
+$ pip install playsound
+from playsound import playsound
+print "(Hello! Type how you're feeling today. Example: Feeling.happy)"
+class Emotions
+    happy = playsound('C:\Users\Home\Documents\GitHub\python-project-wunkerbunker\sounds\happy')
+    #Cmaj
+    sad = playsound()
+    #Cmin7
+    angry = playsound()
+    #Cdim7
+    suspicious = playsound()
+    #CminMaj7
+    triumphant = playsound()
+    #E/DMaj9
+    melancholy = playsound()
+    #Bmaj13
+    romantic = playsound()
+    #CMaj7
+    optimistic = playsound()
+    #C9
+    #To be continued
+Feeling = Emotions()
